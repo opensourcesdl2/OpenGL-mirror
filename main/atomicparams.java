36e41f40f45f3d30585b1de29e4b9395adf9e3c5
opensourcesdl2 <matthewmontalvo925@gmail.com>
2020-12-21 21:36:03 -0500

    Revert "Update atomicparams.java"
    This reverts commit f18be88aedba5553b7561cdac5483d665eac9ac3.

--- main/atomicparams.java
+++
@@ -1,8 +0,0 @@
-glPushMatrix();
-    glScalef(1, -1, 1);  /* "reflect" across Y=0 plane. */
-    setLightSourcePositions();
-    drawNinja();
-  glPopMatrix();
-  setLightSourcePositions();
-  drawNinja();
-  
